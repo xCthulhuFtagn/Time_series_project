e1bced7ef69d6bbf7b3ab975c93ab46d520fd4f5
xCthulhuFtagn <mkolodyazhny@gmail.com>
2024-04-12 23:16:08 +0300

    interface update

--- feature_selection.py
+++ feature_selection.py
@@ -1,6 +1,7 @@
 class FeatureSelector():
-    def __init__(self, X, y, method, model=None):
-        self.X = X
+    def __init__(self, X_endog, X_exog = None, y, method, model=None):
+        self.X_endog = X_endog
+        self.X_exog = X_exog
         self.y = y
         if method in ['method1', 'method2', 'method3'] :
             # тут как раз все нужные методы, что выберем экспериментами
@@ -18,5 +19,6 @@ class FeatureSelector():
             pass
         elif self.method == 'method3':
             pass
-        return sub_X, model
+        if self.X_exog is not None: return sub_X_endog, sub_X_exog, model
+        else: return sub_X_endog, model
     
